8577aebc57addc47fad6470eee99cc3802ffce1e
Farid AL BARAZI <farid.al.barazi@gmail.com>
2022-11-01 15:27:29 +0100

    ok

--- myProject/src/main/java/com/wildcodeschool/myProject/controller/MainController.java
+++ myProject/src/main/java/com/wildcodeschool/myProject/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.wildcodeschool.myProject.controller;
 
+import com.wildcodeschool.myProject.beans.Person;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +17,17 @@ public class MainController {
         return "Greetings from <a href='https://spring.io/projects/spring-boot'>Spring Boot!</a>";
 
 
+    }
+    @GetMapping("/person")
+    @ResponseBody
+    public Person personSample() {
+
+        return new Person("John Doe", 30);
+    }
+
+    @GetMapping("/login")
+    public String signIn() {
+
+        return "sign-in.html";
     }
 }
