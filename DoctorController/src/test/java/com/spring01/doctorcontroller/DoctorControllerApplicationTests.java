703074a06d4f048e145a913d56bdbc0dec760d24
Farid AL BARAZI <farid.al.barazi@gmail.com>
2022-11-01 15:27:47 +0100

    ok

---
+++ DoctorController/src/test/java/com/spring01/doctorcontroller/DoctorControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring01.doctorcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
